508d650579638989825a2c78842d54711c79d881
nicolas <gutierrezn3506@gmail.com>
2025-08-27 14:12:34 -0300

    Criando o Projeto Pizzaria

---
+++ script.sql
@@ -0,0 +1,17 @@
+-- CRIANDO O BANCO DE DADOS --
+CREATE DATABASE BDPizzaria;
+
+-- USANDO BANCO DE DADOS --
+USE DBPizzaria;
+
+-- CRIANDO AS TABELAS DO BANCO DE DADOS --
+CREATE TABLE tbPedido(
+	codPedido int primary key auto_increment,
+    tipoPizza varchar(50),
+    valorPizza varchar(50),
+    valorOpcao varchar(50),
+    valorTotal varchar(50)
+); 
+
+-- CONSULTANDO AS TABELAS DO BANCO
+SELECT * FROM tbPedido;
